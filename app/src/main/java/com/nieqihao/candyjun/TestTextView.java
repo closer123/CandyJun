ee5783d51a475ee3170fbb948d751b2b4de6805b
小浩 <1804946628@qq.com>
2025-01-07 18:53:08 +0800

    自定义组件依赖，测试textView

---
+++ app/src/main/java/com/nieqihao/candyjun/TestTextView.java
@@ -0,0 +1,22 @@
+package com.nieqihao.candyjun;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class TestTextView extends androidx.appcompat.widget.AppCompatTextView {
+    public TestTextView(Context context) {
+        super(context);
+    }
+
+    public TestTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
